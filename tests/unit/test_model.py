134a3274b7ba44eb37ab440ab8b277ff49b21fa0
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2023-09-21 18:49:42 +0200

    fixing bug

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -7,7 +7,7 @@ def test_create_account():
     WHEN a new Account is created
     THEN check the name, account_number, balance, currency, country, status and created_at fields are defined correctly
     """
-    account = Account('John Doe', '€')
+    account = Account('John Doe', '€', 'Argentina')
     assert account.name == 'John Doe'
     assert account.currency == '€'
     assert account.account_number != None
