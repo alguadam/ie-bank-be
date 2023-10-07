9b49133c98afc22c4ba89d4e96de772d130d9dc1
Greta Lerer <gretalerer@gmail.com>
2023-10-08 00:32:16 +0200

    first attempt at putting the be in azure

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -4,25 +4,24 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
-app = Flask(__name__)
-app.app_context().push()
 
+app = Flask(__name__)
 
 load_dotenv()
 
 # Select environment based on the ENV environment variable
-if os.getenv('ENV') == 'local':
+if os.getenv("ENV") == "local":
     print("Running in local mode")
-    app.config.from_object('config.LocalConfig')
-elif os.getenv('ENV') == 'dev':
+    app.config.from_object("config.LocalConfig")
+elif os.getenv("ENV") == "dev":
     print("Running in development mode")
-    app.config.from_object('config.DevelopmentConfig')
-elif os.getenv('ENV') == 'ghci':
+    app.config.from_object("config.DevelopmentConfig")
+elif os.getenv("ENV") == "ghci":
     print("Running in github mode")
-    app.config.from_object('config.GithubCIConfig')
+    app.config.from_object("config.GithubCIConfig")
 else:
     print("Running in production mode")
-    app.config.from_object('config.ProductionConfig')
+    app.config.from_object("config.ProductionConfig")
 
 
 db = SQLAlchemy(app)
--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -10,13 +10,25 @@ def hello_world():
 @app.route('/skull', methods=['GET'])
 def skull():
     return 'Hi! This is the BACKEND SKULL! 💀'
+    text = text + "<br/>Database URL:" + db.engine.url.database
+    if db.engine.url.host:
+        text = text + "<br/>Database host:" + db.engine.url.host
+    if db.engine.url.port:
+        text = text + "<br/>Database port:" + db.engine.url.port
+    if db.engine.url.username:
+        text = text + "<br/>Database user:" + db.engine.url.username
+    if db.engine.url.password:
+        text = text + "<br/>Database password:" + db.engine.url.password
+    return text
+
 
 
 @app.route('/accounts', methods=['POST'])
 def create_account():
     name = request.json['name']
     currency = request.json['currency']
-    account = Account(name, currency)
+    country = request.json['country']
+    account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
