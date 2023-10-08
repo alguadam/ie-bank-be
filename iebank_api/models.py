5a6040323a907e1135b24fa00d416ff5a5bc46dc
Greta Lerer <gretalerer@gmail.com>
2023-10-08 17:19:58 +0200

    trying to deploy country

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -7,16 +7,18 @@ class Account(db.Model):
     name = db.Column(db.String(32), nullable=False)
     account_number = db.Column(db.String(20), nullable=False, unique=True)
     balance = db.Column(db.Float, nullable=False, default = 0.0)
-    currency = db.Column(db.String(1), nullable=False, default="€")
+    currency = db.Column(db.String(1), nullable=False, unique = True)
+    country = db.Column(db.String(20), nullable=False, default="€")
     status = db.Column(db.String(10), nullable=False, default="Active")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Event %r>' % self.account_number
 
-    def __init__(self, name, currency):
+    def __init__(self, name, currency, country):
         self.name = name
         self.account_number = ''.join(random.choices(string.digits, k=20))
         self.currency = currency
+        self.country = country
         self.balance = 0.0
         self.status = "Active"
