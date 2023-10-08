994df3a7df9f32f26492bb1a49ac0e40ac1c70da
Alvaro Guadamillas <alguadam@microsoft.com>
2023-10-08 21:50:55 +0200

    fix: unit testing

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,6 +1,7 @@
 from iebank_api import app
 import pytest
 
+
 def test_get_accounts(testing_client):
     """
     GIVEN a Flask application
@@ -9,6 +10,7 @@ def test_get_accounts(testing_client):
     """
     response = testing_client.get('/accounts')
     assert response.status_code == 200
+
 
 def test_dummy_wrong_path():
     """
@@ -20,13 +22,13 @@ def test_dummy_wrong_path():
         response = client.get('/wrong_path')
         assert response.status_code == 404
 
+
 def test_create_account(testing_client):
     """
     GIVEN a Flask application
     WHEN the '/accounts' page is posted to (POST)
     THEN check the response is valid
     """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
+    response = testing_client.post(
+        '/accounts', json={'name': 'John Doe', 'currency': '€', 'country': 'Spain'})
     assert response.status_code == 200
-
-
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -5,11 +5,12 @@ def test_create_account():
     """
     GIVEN a Account model
     WHEN a new Account is created
-    THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
+    THEN check the name, account_number, balance, currency, country, status and created_at fields are defined correctly
     """
-    account = Account('John Doe', '€')
+    account = Account('John Doe', '€', 'Spain')
     assert account.name == 'John Doe'
     assert account.currency == '€'
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
+    assert account.country == 'Spain'
