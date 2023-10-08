06fb9bf91183e067cad318b2e3ba224262cadbbb
Alvaro Guadamillas <alguadam@microsoft.com>
2023-10-08 21:43:15 +0200

    add: deployment to UAT

--- config.py
+++ config.py
@@ -25,3 +25,12 @@ class DevelopmentConfig(Config):
     dbname=os.getenv('DBNAME')
     )
     DEBUG = True
+
+class UATConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.getenv('DBUSER'),
+    dbpass=os.getenv('DBPASS'),
+    dbhost=os.getenv('DBHOST'),
+    dbname=os.getenv('DBNAME')
+    )
+    DEBUG = False
--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -19,9 +19,9 @@ elif os.getenv('ENV') == 'dev':
 elif os.getenv('ENV') == 'ghci':
     print("Running in github mode")
     app.config.from_object('config.GithubCIConfig')
-else:
-    print("Running in production mode")
-    app.config.from_object('config.ProductionConfig')
+elif os.getenv('ENV') == 'uat':
+    print("Running in UAT mode")
+    app.config.from_object('config.UATConfig')
 
 db = SQLAlchemy(app)
 
@@ -34,7 +34,7 @@ CORS(app)
 from iebank_api import routes
 
 # Initialize Application Insights and force flushing application insights handler after each request
-if(os.getenv('ENV') == 'dev'):
+if(os.getenv('ENV') == 'dev' or os.getenv('ENV') == 'uat'):
     appinsights = AppInsights(app)
     @app.after_request
     def after_request(response):
