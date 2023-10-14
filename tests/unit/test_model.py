15acefdad5ca9d3653c31172ff1e1a59f153bdd9
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2023-10-14 18:28:13 +0200

    TEST

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -13,3 +13,28 @@ def test_create_account():
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
+
+def test_update_account_status():
+    """
+    GIVEN an Account model
+    WHEN the account status is updated
+    THEN check the updated status is reflected in the model
+    """
+    account = Account("John Doe", "Spain", "€")
+    assert account.status == "Active"
+    
+    account.status = "Inactive"
+    assert account.status == "Inactive"
+
+
+def test_update_account_balance():
+    """
+    GIVEN an Account model
+    WHEN the account balance is updated
+    THEN check the updated balance is reflected in the model
+    """
+    account = Account("John Doe", "Spain", "€")
+    assert account.balance == 0.0
+    
+    account.balance = 100.0
+    assert account.balance == 100.0
