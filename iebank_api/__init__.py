6094dcd90a227af100659869b1af613b5be16b97
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2023-10-15 17:06:38 +0200

    XXXX

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from applicationinsights.flask.ext import AppInsights
 
 app = Flask(__name__)
 
@@ -22,6 +23,7 @@ elif os.getenv('ENV') == 'uat':
     print("Running in UAT mode")
     app.config.from_object('config.UATConfig')
 
+
 db = SQLAlchemy(app)
 
 from iebank_api.models import Account
@@ -32,3 +34,10 @@ CORS(app)
 
 from iebank_api import routes
 
+# Initialize Application Insights and force flushing application insights handler after each request
+if(os.getenv('ENV') == 'dev' or os.getenv('ENV') == 'uat'):
+    appinsights = AppInsights(app)
+    @app.after_request
+    def after_request(response):
+        appinsights.flush()
+        return response
